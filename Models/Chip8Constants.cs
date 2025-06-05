b506f65613111622d9c48476ffa770a1ac72af25
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2025-06-05 21:29:16 +0545

    Added model for basic specification

---
+++ Models/Chip8Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8_Emu.Models
+{
+    public class Chip8Constants
+    {
+        public const int MemorySize = 4096;
+        public const int RegisterCount = 16;
+        public const int StackSize = 16;
+        public const int ScreenWidth = 64;
+        public const int ScreenHeight = 32;
+        public const int ProgramStartAddress = 0x200;
+        public const int FontStartAddress = 0x50;
+    }
+}
