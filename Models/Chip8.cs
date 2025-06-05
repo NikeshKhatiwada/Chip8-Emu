9f392331dc841bd0e9a490187558b79ea1f3a687
NikeshKhatiwada <nikeshkhatiwada@tutanota.com>
2025-06-05 22:52:58 +0545

    Added code for Font characters

---
+++ Models/Chip8.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8_Emu.Models
+{
+    public class Chip8
+    {
+        byte[] memory = new byte[Chip8Constants.MemorySize];
+        byte[] registers = new byte[Chip8Constants.RegisterCount];
+        Stack<ushort> stack = new Stack<ushort>(Chip8Constants.StackSize);
+        ushort pc; // Program Counter
+
+    }
+}
